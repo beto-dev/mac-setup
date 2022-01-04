3f8de8c817bb08d0910cb9f9cfd694317c1b1ffe
Alberto Tejos <alb.tejos.dev@gmail.com>
2022-01-04 16:06:59 -0300

    deleted useless symbols

--- nvm.sh
+++ nvm.sh
@@ -2,11 +2,12 @@
 
 echo "Setting nvm config..."
 
-. $(brew --prefix nvm)/nvm.sh
-nvm --version &&
-nvm install node &&
-nvm use node &&
-node -v && npm -v &&
-nvm install node --reinstall-packages-from=node &&
+. "$(brew --prefix nvm)"/nvm.sh
+nvm --version 
+nvm install node
+nvm use node
+node -v
+npm -v
+nvm install node --reinstall-packages-from=node
 
 echo "Done"
