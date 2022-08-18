baa2cb15fde24b708929524e1a5198537711eee6
Alberto Tejos <alb.tejos.dev@gmail.com>
2022-08-17 21:21:22 -0400

    Vscode extentions turned off

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ chmod +x ./zsh.sh
 chmod +x ./git.sh
 chmod +x ./nvm.sh
 chmod +x ./gcp.sh
-chmod +x ./vscode-extensions-install.sh
 chmod +x ./mac.sh
 
 ./brew.sh
@@ -13,5 +12,4 @@ chmod +x ./mac.sh
 ./nvm.sh
 ./zsh.sh
 ./gcp.sh
-./vscode-extensions-install.sh
 ./mac.sh
