20bc3b91f9ecfc7930483bf89365b96c2aeb316b
beto-dev <64882059+beto-dev@users.noreply.github.com>
2024-02-26 18:07:10 -0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -2,12 +2,13 @@
 
 echo "Setting ssh config..."
 
+mkdir -p ~/.ssh && 
 cd ~/.ssh && 
-ssh-keygen -t rsa -b 4096 -C "alb.tejos.dev@gmail.com"
-eval "$(ssh-agent -s)"
-ssh-add -K ~/.ssh/github
-pbcopy < ~/.ssh/github.pub
-touch ~/.ssh/config
+ssh-keygen -t rsa -b 4096 -C "alb.tejos.dev@gmail.com" && 
+eval "$(ssh-agent -s)" && 
+ssh-add -K ~/.ssh/github && 
+pbcopy < ~/.ssh/github.pub && 
+touch ~/.ssh/config && 
 echo 'Host HOSTNAME\nAddKeysToAgent yes\nUseKeychain yes\nIdentityFile ~/.ssh/github' > config
 
 echo "$(<~/.ssh/github.pub)"
